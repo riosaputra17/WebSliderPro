227840ad67bc366588a574771f53cac529c1c2d8
riosaputra17 <riobadrun1721@gmail.com>
2023-12-29 09:46:37 +0700

    menambahkan fitur login multi user

---
+++ materi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Materi</title>
+</head>
+
+<body>
+    <h1>ini adalah materi </h1>
+    <div align="right"><a href="logout.php" class="btn btn-danger">Logout</a></div>
+</body>
+
+</html>
